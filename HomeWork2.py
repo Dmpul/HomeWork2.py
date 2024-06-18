9f2244d99bac6241dc57727d1c3ee0465eb0ddbf
Дмитрий <dmpul@mail.ru>
2024-06-19 01:42:19 +0300

    Add files via upload

---
+++ HomeWork2.py
@@ -0,0 +1,16 @@
+# Количество выполненных ДЗ (запишите значение 12)
+a='Всего задач'
+Number_of_homework_completed=12
+print(a, Number_of_homework_completed)
+# Количество затраченных часов (запишите значение 1.5)
+b='затрачено часов'
+Number_of_hours_spent=1.5
+# Название курса (запишите значение 'Python')
+Well='Python'
+# Время на одно задание (вычислить используя 1 и 2 переменные)
+C='среднее время выполнения'
+Time_per_task=(Number_of_hours_spent/Number_of_homework_completed)
+print(Time_per_task)
+# Выведите на экран(в консоль), используя переменные, следующую строку:
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print('Курс:',Well,',',a,':',Number_of_homework_completed,',',b,':',Number_of_hours_spent, C, Time_per_task, 'часа')
